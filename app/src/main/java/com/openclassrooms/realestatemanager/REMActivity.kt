d3c3861871433500c53f45c49cb8bf7c864b447b
bozbi <bosbizz@gmail.com>
2020-05-25 17:32:18 +0200

    Cleaning project

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/REMActivity.kt
@@ -0,0 +1,12 @@
+package com.openclassrooms.realestatemanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class REMActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_r_e_m)
+    }
+}
